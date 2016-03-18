c0ec2ad4d9b40948f866d9629db14d2bc9f19250
TakeAsh <take@TakeAsh.net>
2016-03-19 01:18:07 +0900

    Disable the buttons that is not set macro.

---
+++ SendKeys/StringToBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace SendKeys {
+
+    public class StringToBoolConverter :
+        IValueConverter {
+
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        ) {
+            return !String.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture
+        ) {
+            throw new NotSupportedException("MethodToValueConverter can only be used for one way conversion.");
+        }
+    }
+}
